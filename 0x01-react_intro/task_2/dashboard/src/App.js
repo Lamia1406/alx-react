e3fb25bbf3472f0ed8a2329e385a26ba160df9ed
Lamia Hamdi <101535283+Lamia1406@users.noreply.github.com>
2024-10-29 21:55:54 +0100

    add login form to app.js

---
+++ 0x01-react_intro/task_2/dashboard/src/App.js
@@ -0,0 +1,41 @@
+import logo from './holberton-logo.jpg';
+import './App.css';
+import { getFooterCopy, getFullYear } from './utils';
+function App() {
+  return (
+    <div className='App-main'>
+      <div className='App-header'>
+        <img src={logo} alt='Holberton Logo' />
+        <h1>School dashboard</h1>
+      </div>
+      <div className='App-body'>
+        <p>Login to access the full dashboard</p>
+        <form>
+          <div className='form-field'>
+          <label htmlFor='email'>Email</label>
+          <input type='email' id='email' placeholder='Please enter your email' />
+          </div>
+          <div className='form-field'>
+          <label htmlFor='password'>Password</label>
+          <input type='password' id='password' placeholder='Please enter your password' />
+          </div>
+          <div className='form-submit'>
+          <button>OK</button>
+          </div>
+        
+          
+  
+        
+            
+         
+        </form>
+      </div>
+      <div className='App-footer'>
+        <p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
+      </div>
+    </div>
+
+  );
+}
+
+export default App;
