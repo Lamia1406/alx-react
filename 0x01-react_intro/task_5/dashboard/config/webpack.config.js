7ef58aa0cbf879919a95b1a40b34d5077683f483
Lamia Hamdi <101535283+Lamia1406@users.noreply.github.com>
2024-10-30 16:30:29 +0100

    create a project using webpack

---
+++ 0x01-react_intro/task_5/dashboard/config/webpack.config.js
@@ -0,0 +1,48 @@
+const path = require("path");
+
+module.exports = {
+    entry: path.resolve(__dirname, '../src/index.js'),
+    output: {
+        path: path.resolve(__dirname, '../dist'),
+        filename: 'bundle.js',
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        static: path.join(__dirname, '../dist'),
+        hot: true,
+        port: 8080,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|jpg|gif|svg)$/i,
+                use: [
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {
+                            mozjpeg: {
+                                progressive: true,
+                                quality: 65,
+                            },
+                            pngquant: {
+                                quality: [0.65, 0.90],
+                                speed: 4,
+                            },
+                            gifsicle: {
+                                interlaced: false,
+                            },
+                            webp: {
+                                quality: 75,
+                            },
+                        },
+                    },
+                    'file-loader',
+                ],
+            },
+        ],
+    },
+};
---
+++ 0x01-react_intro/task_5/dashboard/src/index.js
@@ -0,0 +1,5 @@
+
+const app = document.getElementById('app');
+app.innerHTML = `
+    <h1>Hello, Holberton!</h1>
+`;
