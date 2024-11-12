3aa6183f0e02399e7b80570fa748323293719a0d
Lamia Hamdi <101535283+Lamia1406@users.noreply.github.com>
2024-11-12 13:54:43 +0100

    create bodySection component

---
+++ 0x03-React_component/task_3/dashboard/src/BodySection/BodySection.js
@@ -0,0 +1,7 @@
+import React from "react"
+export default function BodySection({title, children}) {
+    return <div className="BodySection">
+        <h2>{title}</h2>
+        {children}
+    </div>
+}
