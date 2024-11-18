efc62eae0668c974b087dfed54ffd2b12aad6638
Lamia Hamdi <101535283+Lamia1406@users.noreply.github.com>
2024-11-18 16:21:27 +0100

    write login.test.js tests

--- 0x05-react_state/task_1/dashboard/src/Login/Login.test.js
+++ 0x05-react_state/task_1/dashboard/src/Login/Login.test.js
@@ -16,9 +16,18 @@ describe('Login component tests', () => {
     expect(component).toBeDefined();
   });
   it('contains an <input> tag', () => {
-    expect(component.find('input').length).toBe(2);
+    expect(component.find('input').length).toBe(3);
   });
   it('contains an <label> tag', () => {
     expect(component.find('label').length).toBe(2);
   });
+  it('verifies that the submit button is disabled by default', () => {
+		expect(component.find('input').at(2).props().disabled).toEqual(true);
+	});
+
+	it('verifies that after changing the value of the two inputs, the button is enabled', () => {
+		component.find('input').at(0).simulate('change', { target: { name: 'email', value: 'mnortiz.ortiz@gmail.com'} });
+		component.find('input').at(1).simulate('change', { target: { name: 'password', value: '012345'} });
+		expect(component.find('input').at(2).props().disabled).toEqual(false);
+	});
 });
