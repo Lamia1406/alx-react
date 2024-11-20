146947a8e6bf3ffce5f09456d0a85c0622e46b45
Lamia Hamdi <101535283+Lamia1406@users.noreply.github.com>
2024-11-20 10:05:39 +0100

    apply semistandard to file

--- 0x06-React_Immutable/2-nested.js
+++ 0x06-React_Immutable/2-nested.js
@@ -1,4 +1,4 @@
-import {Map} from "immutable"
-export default function accessImmutableObject(object, array) {
-    return Map(object).getIn(array, undefined)
+import { Map } from 'immutable';
+export default function accessImmutableObject (object, array) {
+  return Map(object).getIn(array, undefined);
 }
