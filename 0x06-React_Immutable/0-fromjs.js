8d98241bf178a08d6ebc297c47bd0e48e586f729
Lamia Hamdi <101535283+Lamia1406@users.noreply.github.com>
2024-11-20 10:03:58 +0100

    apply semistandard to file

--- 0x06-React_Immutable/0-fromjs.js
+++ 0x06-React_Immutable/0-fromjs.js
@@ -1,7 +1,6 @@
 
 import { fromJS } from 'immutable';
 
-export default function getImmutableObject(object) {
-    return fromJS(object);
+export default function getImmutableObject (object) {
+  return fromJS(object);
 }
-
