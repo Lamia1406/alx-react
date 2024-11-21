46cd47ab063987c5a6fc7ae00e3202c467820a6b
Lamia Hamdi <101535283+Lamia1406@users.noreply.github.com>
2024-11-21 15:04:35 +0100

    concat two immutable Lists vs merge two immutable lists

---
+++ 0x06-React_Immutable/5-merge.js
@@ -0,0 +1,8 @@
+import { List } from 'immutable';
+
+export function concatElements (page1, page2) {
+  return List(page1).concate(List(page2));
+}
+export function mergeElements (page1, page2) {
+  return List(page1).merge(List(page2));
+}
